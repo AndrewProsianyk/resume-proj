bc5d87d0694ca8e6da8afa7e0eeb45c24da012fd
Andrew <nonameznoone@gmail.com>
2022-02-07 12:58:40 +0200

    code fixed

--- script/script.js
+++ script/script.js
@@ -24,6 +24,43 @@ openFirstModalButton.addEventListener('click', firstModalOpen)
 openSecondModalButton.addEventListener('click', secondModalOpen)
 
 
+function firstModalOpen() {
+    backdropEl.classList.add('is-open')
+    backdropEl.addEventListener('click', onModalBackdropClick)
+    closeFirstModalButton.addEventListener('click', onCloseModalBtnClick);
+    window.addEventListener('keydown', onEscPress)
+    bodyEl.classList.add('backdrop-open')
+    modalFirstEl.classList.add('modal-open')
+}
+function secondModalOpen() {
+    backdropEl.classList.add('is-open')
+    backdropEl.addEventListener('click', onModalBackdropClick)
+    closeSecondModalButton.addEventListener('click', onCloseModalBtnClick);
+    window.addEventListener('keydown', onEscPress)
+    bodyEl.classList.add('backdrop-open')
+    modalSecondEl.classList.add('modal-open')
+}
+
+function onCloseModalBtnClick() {
+    window.removeEventListener('keydown', onEscPress)
+    backdropEl.classList.remove('is-open')
+    bodyEl.classList.remove('backdrop-open')
+    modalFirstEl.classList.remove('modal-open')
+};
+
+function onModalBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+        onCloseModalBtnClick()
+    }
+};
+
+function onEscPress(event) {
+    const ESC_KEY_CODE = 'Escape'
+    if (event.code === ESC_KEY_CODE) {
+        onCloseModalBtnClick()
+    }
+}
+
 function onSliderClick(event) {
     if (!event.target.classList.contains('slide-btn')) {
         return
@@ -37,47 +74,3 @@ function onSliderClick(event) {
         sliderCardEl.classList.remove('open')
     })
 };
-
-function firstModalOpen() {
-    backdropEl.classList.add('is-open')
-    backdropEl.addEventListener('click', onModalBackdropClick)
-    closeFirstModalButton.addEventListener('click', onCloseFirstModalBtnClick);
-    window.addEventListener('keydown', onEscPress)
-    bodyEl.classList.add('backdrop-open')
-    modalFirstEl.classList.add('modal-open')
-}
-function secondModalOpen() {
-    backdropEl.classList.add('is-open')
-    backdropEl.addEventListener('click', onModalBackdropClick)
-    closeSecondModalButton.addEventListener('click', onCloseSecondModalBtnClick);
-    window.addEventListener('keydown', onEscPress)
-    bodyEl.classList.add('backdrop-open')
-    modalSecondEl.classList.add('modal-open')
-}
-
-function onCloseFirstModalBtnClick() {
-    window.removeEventListener('keydown', onEscPress)
-    backdropEl.classList.remove('is-open')
-    bodyEl.classList.remove('backdrop-open')
-    modalFirstEl.classList.remove('modal-open')
-};
-
-function onCloseSecondModalBtnClick() {
-    window.removeEventListener('keydown', onEscPress)
-    backdropEl.classList.remove('is-open')
-    bodyEl.classList.remove('backdrop-open')
-    modalSecondEl.classList.remove('modal-open')
-};
-
-function onModalBackdropClick(event) {
-    if (event.currentTarget === event.target) {
-        onCloseFirstModalBtnClick() || onCloseSecondModalBtnClick()
-    }
-};
-
-function onEscPress(event) {
-    const ESC_KEY_CODE = 'Escape'
-    if (event.code === ESC_KEY_CODE) {
-        onCloseFirstModalBtnClick() || onCloseSecondModalBtnClick()
-    }
-}
